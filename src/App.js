244f612ddf97c8c0b9695b2ffacf8e9994172c98
Ravi_Kumar <ravikumar.ug20@nsut.ac.in>
2024-01-23 23:50:43 +0530

    password_generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [allowedNumber, setAllowedNumber] = useState(false);
+  const [allowedSymbol, setAllowedSymbol] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useRef hook
+  const passwordRef = useRef(null);
+  /// it's used for optimization
+  
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (allowedNumber) str += "0123456789";
+
+    if (allowedSymbol) str += "!@#$%^&*()_+-={}[]:;'<>,.?/~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, allowedNumber, allowedSymbol, setPassword]);
+
+  const copyPasswordToClip = useCallback(() => {
+      passwordRef.current?.select();
+      passwordRef.current?.setSelectionRange(0, 50);
+      window.navigator.clipboard.writeText(password)
+  }, [password])
+  
+  useEffect(() => {
+    generatePassword();
+    // let pass = "";
+    // let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    // if (allowedNumber) str += "0123456789";
+
+    // if (allowedSymbol) str += "!@#$%^&*()_+-={}[]:;'<>,.?/~`";
+
+    // for (let i = 1; i <= length; i++) {
+    //   let char = Math.floor(Math.random() * str.length + 1);
+    //   pass += str.charAt(char);
+    // }
+
+    // setPassword(pass);
+  }, [length, allowedNumber, allowedSymbol, generatePassword]);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-5 py-2 my-8 text-orange-500 bg-gray-700">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button 
+            onClick={copyPasswordToClip}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={8}
+              max={50}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={allowedNumber}
+              id="numberInput"
+              onChange={() => {
+                setAllowedNumber((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numperInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={allowedSymbol}
+              id="numberInput"
+              onChange={() => {
+                setAllowedSymbol((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numperInput">Symbols</label>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
+
 
 export default App;
